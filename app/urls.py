2085269f4eda76a1589629433c983168e536a580
Katie Lachance <katherine.c.lachance.15@dartmouth.edu>
2015-01-12 16:55:46 -0500

    More signup

--- app/urls.py
+++ app/urls.py
@@ -5,5 +5,6 @@ from app import views
 urlpatterns = patterns('',
     # splash page
     url(r'^$', views.splash, name='splash'),
-
+    # sign up page
+    url(r'^signup/$', views.sign_up, name='sign_up'),
 )

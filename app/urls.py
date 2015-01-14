851dd01bb78d7c3b4cc61dcd1accd2570ee2f103
Katie Lachance <katherine.c.lachance.15@dartmouth.edu>
2015-01-14 16:17:06 -0500

    added toolbar to bottom of page

--- app/urls.py
+++ app/urls.py
@@ -7,4 +7,8 @@ urlpatterns = patterns('',
     url(r'^$', views.splash, name='splash'),
     # sign up page
     url(r'^signup/$', views.sign_up, name='sign_up'),
+    # how to page
+    url(r'^howto/$', views.howto, name='howto'),
+    # about us page
+    url(r'^aboutus/$', views.aboutus, name='aboutus'),
 )
--- app/views.py
+++ app/views.py
@@ -6,3 +6,9 @@ def splash(request):
 
 def sign_up(request):
     return render(request, 'app/sign_up.html', {})
+
+def howto(request):
+    return render(request, 'app/howto.html', {})
+
+def aboutus(request):
+    return render(request, 'app/aboutus.html', {})

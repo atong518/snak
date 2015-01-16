2317bfbfc8f8bb187db771e8c93ce14e9291f5a8
Diana Salsbury <diana.salsbury@gmail.com>
2015-01-16 15:33:31 -0500

    create abstract form for genericUser and then child forms for ProspieUser and CollegeUser respectively

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,10 @@
 from django.forms import ModelForm, EmailInput, TextInput, PasswordInput, Textarea, Select
 from django.contrib.auth.forms import UserCreationForm
-from app.models import CollegeUser, ProspieUser
+from app.models import GenericUser, CollegeUser, ProspieUser
 
-class CollegeSignupForm(UserCreationForm):
+class GenericSignupForm(UserCreationForm):
     class Meta:
-        model = CollegeUser
+        model = GenericUser
         fields = ['email', 
                   'password1', 
                   'password2', 
@@ -12,49 +12,7 @@ class CollegeSignupForm(UserCreationForm):
                   'lastname', 
                   'homecountry',
                   'homestate',
-                  'bio']
-
-    def __init__(self, *args, **kwargs):
-        super(CollegeSignupForm, self).__init__(*args, **kwargs)
-        self.fields['firstname'].widget = TextInput(attrs={
-            'class': 'form-control',
-            'placeholder': 'First Name'})
-        self.fields['lastname'].widget = TextInput(attrs={
-            'class': 'form-control',
-            'placeholder': 'Last Name'})
-        self.fields['email'].widget = EmailInput(attrs={
-            'class': 'form-control',
-            'placeholder': 'Email'})
-        self.fields['password1'].widget = PasswordInput(attrs={
-            'class': 'form-control',
-            'placeholder': 'Password'})
-        self.fields['password2'].widget = PasswordInput(attrs={
-            'class': 'form-control',
-            'placeholder': 'Password again'})
-        self.fields['homecountry'].widget = Select(attrs={
-             'id': 'countrySelect1',
-             'name': 'country',
-             'onchange': "populateState(\'stateSelect1\', \'countrySelect1\')",
-             'class': 'form-control'})
-        self.fields['homestate'].widget = Select(attrs={
-             'id': 'stateSelect1',
-             'name': 'state',
-             'class': 'form-control'})
-        self.fields['bio'].widget = Textarea(attrs={
-            'class': 'form-control',
-            'placeholder': 'Tell us about yourself!'})
-        self.fields.pop('username')
-
-class ProspieSignupForm(UserCreationForm):
-    class Meta:
-        model = ProspieUser
-        fields = ['email', 
-                  'password1', 
-                  'password2', 
-                  'firstname', 
-                  'lastname', 
-                  'homecountry',
-                  'homestate']
+                 ]
 
     def clean(self):
         cleaned_data = super(ProspieSignupForm, self).clean()
@@ -70,7 +28,7 @@ class ProspieSignupForm(UserCreationForm):
         return cleaned_data
 
     def __init__(self, *args, **kwargs):
-        super(ProspieSignupForm, self).__init__(*args, **kwargs)
+        super(GenericSignupForm, self).__init__(*args, **kwargs)
         self.fields['firstname'].widget = TextInput(attrs={
             'class': 'form-control',
             'placeholder': 'First Name'})
@@ -86,6 +44,37 @@ class ProspieSignupForm(UserCreationForm):
         self.fields['password2'].widget = PasswordInput(attrs={
             'class': 'form-control',
             'placeholder': 'Password again'})
+        self.fields.pop('username')
+
+
+class CollegeSignupForm(GenericSignupForm):
+    class Meta(GenericSignupForm.Meta):
+        model = CollegeUser
+        fields = GenericSignupForm.Meta.fields + ['bio']
+
+    def __init__(self, *args, **kwargs):
+        super(CollegeSignupForm, self).__init__(*args, **kwargs)
+        self.fields['homecountry'].widget = Select(attrs={
+             'id': 'countrySelect1',
+             'name': 'country',
+             'onchange': "populateState(\'stateSelect1\', \'countrySelect1\')",
+             'class': 'form-control'})
+        self.fields['homestate'].widget = Select(attrs={
+             'id': 'stateSelect1',
+             'name': 'state',
+             'class': 'form-control'})
+        self.fields['bio'].widget = Textarea(attrs={
+            'class': 'form-control',
+            'placeholder': 'Tell us about yourself!'})
+
+class ProspieSignupForm(GenericSignupForm):
+    class Meta(GenericSignupForm.Meta):
+        model = ProspieUser
+        fields = GenericSignupForm.Meta.fields
+
+    def __init__(self, *args, **kwargs):
+        super(ProspieSignupForm, self).__init__(*args, **kwargs)
+
         self.fields['homecountry'].widget = Select(attrs={
             'id': 'countrySelect2',
             'name': 'country',
@@ -95,4 +84,3 @@ class ProspieSignupForm(UserCreationForm):
             'id': 'stateSelect2',
             'name': 'state',
             'class': 'form-control'})
-        self.fields.pop('username')

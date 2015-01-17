70097fa8af4d8fbb99d07cf58f9a3034284c9bed
Sam <Samuel.A.Farid.15@dartmouth.edu>
2015-01-17 13:40:00 -0500

    small changes to models

--- snakd/apps/interest/models.py
+++ snakd/apps/interest/models.py
@@ -2,4 +2,6 @@ from django.db import models
 
 class Interest(models.Model):
 
-	name = models.CharField(max_length=20)
+	name = models.CharField(max_length = 20)
+	parent = models.ForeignKey('self')
+	children = models.ManyToManyField('self', symmetrical = False)
--- snakd/apps/user/models.py
+++ snakd/apps/user/models.py
@@ -1,8 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+#from django.contrib.auth.models import AbstractBaseUser
 
-
-# temp user model
 class User(models.Model):
 
 	name = models.CharField(max_length=20)

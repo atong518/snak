44dfd9f6792f1fd63984cadda97b36258683ba65
Nick Joung <njoung@yahoo.com>
2015-01-17 14:40:51 -0500

    merging in config scripts

---
+++ scripts/db_data/create_db.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+DBDATA_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ROOT_DIR=$DBDATA_DIR/../..
+
+echo "CREATING DB..."
+createuser snakd -s
+createdb master -O snakd -w
+
+echo "MIGRATING..."
+python $ROOT_DIR/manage.py migrate
+
+

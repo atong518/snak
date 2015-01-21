46e3bac67e7cad77b4e99a7f6ec1a45398ba8e6d
Sam <Samuel.A.Farid.15@dartmouth.edu>
2015-01-21 16:47:48 -0500

    interest model with build subtree

--- snakd/apps/interest/models.py
+++ snakd/apps/interest/models.py
@@ -1,7 +1,25 @@
 from django.db import models
+from djutils import decorators.memoize
 
 class Interest(models.Model):
 
-	name = models.CharField(max_length = 20)
-	parent = models.ForeignKey('self')
-	children = models.ManyToManyField('self', symmetrical = False)
+	name = models.CharField(max_length = 20, blank = False)
+	tooltip models.CharField(max_length = 20, null = True) # or blank=True?
+
+	comp_weight = models.IntegerField(default = 0)
+
+	parent = models.ManyToManyField("self", symmetrical = False)
+
+
+	@memoize
+	def build_subtree(self, ID):
+		if ID.parent == None:
+			return tree.make_head()
+
+		tree.add( build_subtree(ID.parent) )
+		
+		return tree
+
+
+	def __str__(self):
+		return self.name

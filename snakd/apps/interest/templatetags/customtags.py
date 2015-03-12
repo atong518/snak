122125e5e544a437d2b00d1c32014765951c941b
Nick Joung <njoung@yahoo.com>
2015-03-12 15:47:16 -0400

    semi working version of interests page

--- snakd/apps/interest/templatetags/customtags.py
+++ snakd/apps/interest/templatetags/customtags.py
@@ -1,13 +1,12 @@
 from django import template
+import json
 
 register = template.Library()
 
 @register.filter(name='loadsjs')
 def loadsjs(value):
-	import json
 	return json.loads(value)
 
 @register.filter(name='dumpsjs')
 def loadsjs(value):
-	import json
 	return json.dumps(value)

93dfda8ecabec50d5d973cbe2f36aeff85bba055
Nick Joung <njoung@yahoo.com>
2015-04-01 17:05:38 -0400

    dsf

--- snakd/wsgi.py
+++ snakd/wsgi.py
@@ -7,11 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "snakd.settings")
+# import os
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "snakd.settings")
 
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
 application = Cling(get_wsgi_application())
-import pdb; pdb.set_trace()

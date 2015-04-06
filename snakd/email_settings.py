0bff8b9873a410ce8ef0bb1b89c5512963443cbb
Diana Salsbury <diana.salsbury@gmail.com>
2015-04-05 20:27:09 -0400

    ugh

---
+++ snakd/email_settings.py
@@ -0,0 +1,7 @@
+# THIS FILE SHOULD NOT BE PUSHED TO GIT
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'sagelyio@gmail.com'
+EMAIL_HOST_PASSWORD = 'stop hacking us you weirdoes'
+EMAIL_PORT = 587

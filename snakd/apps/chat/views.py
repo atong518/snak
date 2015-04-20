ff1f6a57d300cf9aeec63eb29bdca3feae3e74f2
Nick Joung <njoung@yahoo.com>
2015-04-20 16:07:51 -0400

    testing

--- snakd/apps/chat/views.py
+++ snakd/apps/chat/views.py
@@ -12,6 +12,21 @@ from datetime import datetime, timedelta
 
 from snakd.apps.user.views import login
 
+# list of mobile User Agents
+mobile_uas = [
+    'w3c ','acs-','alav','alca','amoi','audi','avan','benq','bird','blac',
+    'blaz','brew','cell','cldc','cmd-','dang','doco','eric','hipt','inno',
+    'ipaq','java','jigs','kddi','keji','leno','lg-c','lg-d','lg-g','lge-',
+    'maui','maxo','midp','mits','mmef','mobi','mot-','moto','mwbp','nec-',
+    'newt','noki','oper','palm','pana','pant','phil','play','port','prox',
+    'qwap','sage','sams','sany','sch-','sec-','send','seri','sgh-','shar',
+    'sie-','siem','smal','smar','sony','sph-','symb','t-mo','teli','tim-',
+    'tosh','tsm-','upg1','upsi','vk-v','voda','wap-','wapa','wapi','wapp',
+    'wapr','webc','winw','winw','xda','xda-'
+    ]
+ 
+mobile_ua_hints = [ 'SymbianOS', 'Opera Mini', 'iPhone' ]
+
 def mobileBrowser(request):
     ''' Super simple device detection, returns True for mobile devices '''
  

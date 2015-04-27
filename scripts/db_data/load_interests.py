e7a3e74561fc1f8c4ca054044619a0d4a9aa24be
Sam <sfarid93@gmail.com>
2015-04-27 16:58:59 -0400

    how to edit db interests without deleting literally everything

--- scripts/db_data/load_interests.py
+++ scripts/db_data/load_interests.py
@@ -21,24 +21,6 @@ dic = {
 # create the root
 root_node, created = Interest.objects.get_or_create(hidden=True, **dic)
 
-# test code for creating dummy interests
-# count = 0
-# for name in ["Academics", "Athletics", "Extracurriculars"]:
-# 	dic["name"] = name
-# 	dic["parent"] = i
-# 	x, created = Interest.objects.get_or_create(**dic)
-# 	for j in range(0, 4):
-# 		dic["name"] = count.__str__()
-# 		dic["parent"] = x
-# 		y, created = Interest.objects.get_or_create(**dic)
-# 		count += 1
-# 		for k in range(0, 3):
-# 			dic["name"] = count.__str__()
-# 			dic["parent"] = y
-# 			z, created = Interest.objects.get_or_create(**dic)
-# 			count += 1
-
-
 def build_subtree(parent, **kwargs):
 	children = kwargs.pop("children", [])
 	kwargs["parent"] = parent
@@ -57,4 +39,37 @@ with open(os.path.join(FILE, "interests.json")) as jsonfile:
 		build_subtree(root_node, **child)
 
 buildMatrix()
+
+'''
+Editing interests test cases:
+1) add a new interest
+get_or_create() takes care of this
+already built into the algorithm
+
+2) delete an interest
+deleted interest will be left out of new dist matrix, but will need to be deleted from the 
+	database and removed from each user who has chosen it
+interest_to_delete.user_set.all() gets the users who have chosen it
+
+3) change a single interest name (assume no tooltip or any other identifier)
+I think this has to happen manually
+BEFORE running load_interests.py,
+./manage.py shell
+from snakd.apps.interest.models import Interest
+i = Interest.objects.get_or_create(name="old_name")
+i.name = "new_name"
+i.save()
+NOW that the name is changed in the db, the json file can be reloaded
+
+4) two interests with the same name, change a tooltip
+try:
+	get_or_create(name="name_of_two_interests")
+except Interest.MultipleObjectsReturned:
+	get_or_create(tooltip="tooltip_of_one")
+ACTUALLY I think this is just more easily done manually as well
+
+5) two interests had the same name, one was changed in json
+do step 3 first, but use tooltip or parent as an identifier in get_or_create()
+can double check with id field: id = models.AutoField(primary_key=True)
+'''
 
